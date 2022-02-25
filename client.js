9cb947c21bb850c6556a3aa59cb3b4c702a36a42
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-02-25 05:29:56 +0000

    Add ability for user to enter the file path in CLI where the data comes from

--- client.js
+++ client.js
@@ -1,20 +1,26 @@
-const fs = require('fs');
-const net = require('net');
-
-const conn = net.createConnection({ 
-  host: 'localhost',
-  port: 3000
-});
-conn.setEncoding('utf8'); // interpret data as text
-
-conn.on('connect',() => {
-  console.log('Connected to the server.');
-  fs.readFile('./data.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.log(err.message);
-    } else {
-      console.log(data);
-    }
-    conn.end();
+const fileServer = (loc) => {
+  const fs = require('fs');
+  const net = require('net');
+  
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 3000
   });
-});
+  conn.setEncoding('utf8'); // interpret data as text
+  
+  conn.on('connect',() => {
+    console.log('Connected to the server.');
+    fs.readFile(loc[0], 'utf8', (err, data) => {
+      if (err) {
+        console.log(err.message);
+      } else {
+        console.log(data);
+      }
+      conn.end();
+    });
+  });
+};
+
+const location = process.argv.slice(2);
+
+fileServer(location);
