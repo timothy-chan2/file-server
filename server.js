539b0db41d30855635cd07ea092bcd42b3268c16
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-02-25 04:25:27 +0000

    Add functions to run once connected to the server and data is received

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ const conn = net.createConnection({
   port: 80
 });
 conn.setEncoding('UTF8'); // interpret data as text
+
+conn.on('connect',function(){
+  console.log('client is connected to server.');
+  });
+  
+conn.on('data', function(message){ // this is how we receive
+  console.log(`File says: ${message}`);
+  });
+
+conn.on('end', function(){
+  console.log('Disconnected from the server.');
+  }); 
