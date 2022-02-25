e87841abf361b7e6688640d2c99ea0543d305c1c
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-02-25 04:59:24 +0000

    Add code to read the data in data.txt file and display in the console

--- client.js
+++ client.js
@@ -7,20 +7,16 @@ const conn = net.createConnection({
 });
 conn.setEncoding('utf8'); // interpret data as text
 
-// conn.on('connect',function(){
-//   console.log('Connected to the server.');
-//   fs.readFile('/data', 'utf8', function(err, data){
-//     if (err) {
-//       return console.log(err);
-//     }
-//     console.log(data);
-//   });
-// });
-  
-// // conn.on('data', function(message){ // this is how we receive
-// //   console.log(`File says: ${message}`);
-// //   });
+conn.on('connect',function(){
+  console.log('Connected to the server.');
+  fs.readFile('./data.txt', 'utf8', function(err, data){
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+  });
+});
 
-// conn.on('end', function(){
-//   console.log('Disconnected from the server.');
-//   }); 
+conn.on('end', function(){
+  console.log('Disconnected from the server.');
+}); 
