02501c6f9c15ed89485e4c9a979ee2f0696a46a5
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-02-25 04:05:39 +0000

    Add net connection parameters

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const fs = require('fs');
 const net = require('net');
+
+const conn = net.createConnection({ 
+  host: 'localhost',
+  port: 80
+});
+conn.setEncoding('UTF8'); // interpret data as text
